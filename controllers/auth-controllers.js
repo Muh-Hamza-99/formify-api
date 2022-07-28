73576b8266ea7d3f0852c1d6557653e6902bc60e
muhammad-hamza <Muh-Hamza-99>
2022-07-28 16:14:39 +0300

    Login controller

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -8,3 +8,11 @@ const register = catchAsync(async (req, res, next) => {
     const user = await prisma.user.create({ username, email, password });
     res.status(201);
 }); 
+
+const login = catchAsync(async (req, res, next) => {
+    const { email, password } = req.body;
+    if (!email || !password) return next(new AppError("Please provide an email and password!", 400));
+    const user = await prisma.user.findUnique({ where: { email} });
+    if (!user) return next(new AppError("Incorrect email/password!", 401));
+    res.status(200);
+});
