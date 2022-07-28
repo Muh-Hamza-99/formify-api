3f52b6bcd50ac939fa65819d681df0233293112d
muhammad-hamza <Muh-Hamza-99>
2022-07-28 16:05:00 +0300

    Creating one instance off PrismaClient due to memory leak and overload on server

--- controllers/message-controllers.js
+++ controllers/message-controllers.js
@@ -1,30 +1,28 @@
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
-const Message = prisma.message;
+const prisma = require("../database/prisma");
 
 const AppError = require("../utilities/app-error");
 const catchAsync = require("../utilities/catch-async");
 
 const getAllMessages = catchAsync(async (req, res, next) => {
-    const messages = await Message.findMany({});
+    const messages = await prisma.message.findMany({});
     res.status(200).json({ status: "success", results: messages.length, data: { messages }});
 });
 
 const getOneMessage = catchAsync(async (req, res, next) => {
     const { id } = req.params;
-    const message = await Message.findUnique({ where: { id: Number(id) } });
+    const message = await prisma.message.findUnique({ where: { id: Number(id) } });
     if (!message) return next(new AppError("No message with the provided ID!", 404));
     res.status(200).json({ status: "success", data: { message }});
 });
 
 const createMessage = catchAsync(async (req, res, next) => {
-    const message = await Message.create(req.body);
+    const message = await prisma.message.create(req.body);
     res.status(201).json({ status: "success", data: { message } });
 });
 
 const deleteMessage = catchAsync(async (req, res, next) => {
     const { id } = req.params;
-    const message = await Message.delete({ where: { id: Number(id) } }); 
+    const message = await prisma.message.delete({ where: { id: Number(id) } }); 
     if (!message) return next(new AppError("No message with the provided ID!", 404));
     res.status(204).json({ status: "success", data: null });
 });
--- controllers/route-controllers.js
+++ controllers/route-controllers.js
@@ -1,30 +1,28 @@
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
-const Route = prisma.route;
+const prisma = require("../database/prisma");
 
 const AppError = require("../utilities/app-error");
 const catchAsync = require("../utilities/catch-async");
 
 const getAllRoutes = catchAsync(async (req, res, next) => {
-    const routes = await Route.findMany({});
+    const routes = await prisma.route.findMany({});
     res.status(200).json({ status: "success", results: routes.length, data: { routes }});
 });
 
 const getOneRoute = catchAsync(async (req, res, next) => {
     const { id } = req.params;
-    const route = await Route.findUnique({ where: { id: Number(id) }, include: { messages: true } });
+    const route = await prisma.route.findUnique({ where: { id: Number(id) }, include: { messages: true } });
     if (!route) return next(new AppError("No route with the provided ID!", 404));
     res.status(200).json({ status: "success", data: { route }});
 });
 
 const createRoute = catchAsync(async (req, res, next) => {
-    const route = await Route.create(req.body);
+    const route = await prisma.route.create(req.body);
     res.status(201).json({ status: "success", data: { route } });
 });
 
 const deleteRoute = catchAsync(async (req, res, next) => {
     const { id } = req.params;
-    const route = await Route.delete({ where: { id: Number(id) } }); 
+    const route = await prisma.route.delete({ where: { id: Number(id) } }); 
     if (!route) return next(new AppError("No route with the provided ID!", 404));
     res.status(204).json({ status: "success", data: null });
 });
