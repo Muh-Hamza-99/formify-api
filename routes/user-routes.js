309e29eaaacd715cedfffda44b54e29123229627
muhammad-hamza <Muh-Hamza-99>
2022-07-28 16:22:15 +0300

    Fix: wrong placement of register and login routes

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
+const {
+    register,
+    login,
+} = require("../controllers/auth-controllers");
+
 const { 
     getAllUsers,
     getOneUser,
     createUser,
     updateUser,
     deleteUser,
- } = require("../controllers/user-controllers");
+} = require("../controllers/user-controllers");
+
+router.post("/register", register);
+router.post("/login", login);
 
 router
     .route("/")
