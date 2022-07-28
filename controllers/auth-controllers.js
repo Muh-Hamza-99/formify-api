97eb897cb5c0785cda1c5d0b337cd3e46a738fc9
muhammad-hamza <Muh-Hamza-99>
2022-07-28 18:18:33 +0300

    Re-using the validation schemas

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -5,6 +5,7 @@ const prisma = require("../database/prisma");
 const AppError = require("../utilities/app-error");
 const catchAsync = require("../utilities/catch-async");
 const hashPassword = require("../utilities/hash-password");
+const { registerSchema, loginSchema } = require("../utilities/joi-validation");
 
 const signToken = id => JWT.sign({ id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_TOKEN_EXPIRES_IN });
 
@@ -21,13 +22,17 @@ const createSendToken = (user, statusCode, res) => {
 };
 
 const register = catchAsync(async (req, res, next) => {
-    const { username, email, password } = req.body;
+    const { error, value } = registerSchema.validate(req.body);
+    if (error) return next(new AppError("Validation error!", 403));
+    const { username, email, password } = value;
     const user = await prisma.user.create({ data: { username, email, password: (await hashPassword(password)) } });
     createSendToken(user, 201, res);
 }); 
 
 const login = catchAsync(async (req, res, next) => {
-    const { email, password } = req.body;
+    const { error, value } = registerSchema.validate(req.body);
+    if (error) return next(new AppError("Validation error!", 403));
+    const { email, password } = value;
     if (!email || !password) return next(new AppError("Please provide an email and password!", 400));
     const user = await prisma.user.findUnique({ where: { email } });
     if (!user) return next(new AppError("Incorrect email/password!", 401));
