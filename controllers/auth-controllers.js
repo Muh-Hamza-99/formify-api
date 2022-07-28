f7e62a348692791dbce610d58565414be9e0eaf3
muhammad-hamza <Muh-Hamza-99>
2022-07-28 16:18:02 +0300

    Sending respone of a JWT token using helper functions

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -1,12 +1,28 @@
+const JWT = require("jsonwebtoken");
+
 const prisma = require("../database/prisma");
 
 const AppError = require("../utilities/app-error");
 const catchAsync = require("../utilities/catch-async");
 
+const signToken = id => JWT.sign({ id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_TOKEN_EXPIRES_IN });
+
+const createSendToken = (user, statusCode, res) => {
+    const token = signToken(user.id);
+    const cookieOptions = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production" ? true : false,
+    };
+    res.cookie("jwt", token, cookieOptions);
+    user.password = undefined;
+    res.status(statusCode).json({ status: "success", token });
+};
+
 const register = catchAsync(async (req, res, next) => {
     const { username, email, password } = req.body;
     const user = await prisma.user.create({ username, email, password });
-    res.status(201);
+    createSendToken(user, 201, res);
 }); 
 
 const login = catchAsync(async (req, res, next) => {
@@ -14,5 +30,5 @@ const login = catchAsync(async (req, res, next) => {
     if (!email || !password) return next(new AppError("Please provide an email and password!", 400));
     const user = await prisma.user.findUnique({ where: { email} });
     if (!user) return next(new AppError("Incorrect email/password!", 401));
-    res.status(200);
+    createSendToken(user, 200, res);
 });
