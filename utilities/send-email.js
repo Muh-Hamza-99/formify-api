2425e417f0e8ac39a68000337d9ec505de1d5ed6
muhammad-hamza <Muh-Hamza-99>
2022-07-31 17:11:07 +0300

    Setting up email service using nodemailer

---
+++ utilities/send-email.js
@@ -0,0 +1,20 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = async (options) => {
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.EMAIL_USERNAME,
+            pass: process.env.EMAIL_PASSWORD,
+        },
+    });
+    const mailOptions = {
+        from: "Formify",
+        to: options.email,
+        subject: options.subject,
+        text: options.message,
+    };
+    await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
