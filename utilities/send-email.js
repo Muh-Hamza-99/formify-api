9a56a546d67fd786f3c00c503b311d7e08af166c
muhammad-hamza <Muh-Hamza-99>
2022-08-01 09:39:46 +0300

    Trying to switch email service from nodemailer to Sendgrid

--- utilities/send-email.js
+++ utilities/send-email.js
@@ -1,20 +1,33 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
+
+// const sendEmail = async (options) => {
+//     const transporter = nodemailer.createTransport({
+//         service: "gmail",
+//         auth: {
+//             user: process.env.EMAIL_USERNAME,
+//             pass: process.env.EMAIL_PASSWORD,
+//         },
+//     });
+//     const mailOptions = {
+//         from: "Formify",
+//         to: options.email,
+//         subject: options.subject,
+//         text: options.message,
+//     };
+//     await transporter.sendMail(mailOptions);
+// };
+
+const SendGrid = require("@sendgrid/mail");
+SendGrid.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendEmail = async (options) => {
-    const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASSWORD,
-        },
-    });
-    const mailOptions = {
-        from: "Formify",
+    const email = {
         to: options.email,
+        from: "amazon-price-checker@example.com",
         subject: options.subject,
-        text: options.message,
-    };
-    await transporter.sendMail(mailOptions);
+        text: options.body,
+    };  
+    return SendGrid(email);
 };
 
 module.exports = sendEmail;
