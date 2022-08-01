38694b9c1ac8daabddbbbdd085ac93d0b3954e0e
muhammad-hamza <Muh-Hamza-99>
2022-08-01 13:56:20 +0300

    Installation of security and CORS packages and implemented as middleware

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+const expressRateLimit = require("express-rate-limit");
+const XSS = require("xss-clean");
+const helmet = require("helmet");
+
+const CORS = require("cors");
+
 const AppError = require("./utilities/app-error");
 const globalErrorHandler = require("./middleware/error-handler");
 
@@ -11,11 +17,24 @@ const messageRouter = require("./routes/message-routes.js");
 const routeRouter = require("./routes/route-routes.js");
 const userRouter = require("./routes/user-routes.js");
 
+const limiter = expressRateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: "Too many requests from this device! Please try again after an hour!",
+});
+
+app.options("*", CORS());
+
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 app.use(express.static(__dirname + "/public"));
 
 app.set("view engine", "ejs");
+
+app.use(XSS());
+app.use(helmet());
+
+app.use("/api", limiter);
 
 // Router Middleware
 app.use("/api/v1/messages", messageRouter);
