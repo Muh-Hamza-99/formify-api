3cdb546e0164253d6f927e1f7b96b9a148ba47b2
muhammad-hamza <Muh-Hamza-99>
2022-08-01 12:01:38 +0300

    Adding user role to request object

--- middleware/restrict-to-admin.js
+++ middleware/restrict-to-admin.js
@@ -4,7 +4,10 @@ const AppError = require("../utilities/app-error");
 const catchAsync = require("../utilities/catch-async");
 
 const restrictToAdmin = catchAsync(async (req, res, next) => {
-    if ((req.user.username === process.env.ADMIN_USERNAME) && (req.user.email === process.env.ADMIN_EMAIL) && (await correctPassword(process.env.ADMIN_PASSWORD, req.user.password))) return next();
+    if ((req.user.username === process.env.ADMIN_USERNAME) && (req.user.email === process.env.ADMIN_EMAIL) && (await correctPassword(process.env.ADMIN_PASSWORD, req.user.password))) {
+        req.user.isAdmin = true;
+        return next();
+    };
     return next(new AppError("You do no sufficient permissions in order to be allowed to access this route!", 403));
 });
 
